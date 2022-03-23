d8c49419cc40c86b165911b8614ac08a0b86a8ca
gywlsp <gywls517@gmail.com>
2022-03-23 21:45:29 +0900

    feat: :sparkles: main.dart에서 flutter app id 설정

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,16 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
 
@@ -32,7 +35,16 @@ class _WebViewExampleState extends State<MyApp> {
   void initState() {
     super.initState();
     if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+    initPlatformState();
     FlutterNativeSplash.remove();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    if (!mounted) return;
+
+    String onesignalAppId = dotenv.get('ONESIGNAL_APP_ID');
+    OneSignal.shared.setAppId(onesignalAppId);
   }
 
   @override
