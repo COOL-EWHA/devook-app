8ad51921f1a964ff4e5d0494b8d6a433498c2a4a
gywlsp <gywls517@gmail.com>
2022-03-26 15:21:15 +0900

    build(iOS): :wrench: onesignal ios 설정

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by 박효진 on 2022/03/26.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
