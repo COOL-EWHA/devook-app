a5784016b57ec36b4e19fc0a9fa73fc2744960f8
gywlsp <gywls517@gmail.com>
2022-03-28 07:59:06 +0900

    feat: :sparkles: 기기 등록 함수 추가

---
+++ lib/modules/device.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+
+Future<void> addDevice(String accessToken, FlutterSecureStorage storage) async {
+  final apiHost = dotenv.get('API_HOST');
+  final onesignalAppId = dotenv.get(
+      Platform.isIOS ? 'ONESIGNAL_IOS_APP_ID' : 'ONESIGNAL_ANDROID_APP_ID');
+  final deviceType = Platform.isIOS ? "0" : "1";
+  final response = await http.post(
+    Uri.parse("$apiHost/devices"),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer $accessToken'
+    },
+    body: jsonEncode(
+        <String, String>{'appId': onesignalAppId, 'deviceType': deviceType}),
+  );
+  if (response.statusCode == 200) {
+    var deviceId = jsonDecode(response.body)['deviceId'];
+    await storage.write(key: "deviceId", value: deviceId);
+  }
+}
